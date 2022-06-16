6f135c74e85e81f69b2397d4d3f18254deb797ce
mateussantanasilva <santanasilva1778@gmail.com>
2022-06-16 11:32:27 -0300

    Adicionado o efeito de verificar qual seção está

--- assets/main.js
+++ assets/main.js
@@ -7,8 +7,37 @@ onScroll()
 
 function onScroll() {
     changeColorNavOnScroll()
+
     showBtnBackToTopOnScroll()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection(section) {
+    
+    const targetLine = scrollY + innerHeight / 2
+
+    const topSection = section.offsetTop
+    const heightSection = section.offsetHeight
+    const sectionTopReachOrPassedTargetLine = topSection <= targetLine 
+
+    const bottomSection = topSection + heightSection
+    const bootomSectionNotPassedTargetLine = bottomSection >= targetLine
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && bootomSectionNotPassedTargetLine
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`#menu a[href*=${sectionId}]`)
+    
+    menuElement.classList.remove('active')
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+}
+
 
 function changeColorNavOnScroll(){
     if (scrollY > 0) {
@@ -35,27 +64,150 @@ function closeMenu() {
 }
 
 /* Scroll reveal */
-ScrollReveal({
-    origin: 'top',
-    distance: '40px',
-    duration: 700
-}).reveal(`
-    #home,
-    #home header,
-    #home .content,
-    #home img,
-    #home #statis,
-    #services,
-    #services header,
-    #services .card,
-    #about,
-    #about header,
-    #about .content p,
-    #about .content img,
-    #contact,
-    #contact header,
-    #contact .content #info-contacts,
-    #contact .content a,
-    #contact .content img,
-    footer .wrapper
-`);
+
+
+let screenWidth = window.screen.widt
+
+if(window.screen.width >= 1024){
+
+    console.log('maior tela');
+
+    //Scroll home
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #home header
+    `)
+
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 800
+    }).reveal(`
+        #home .content p
+    `)
+
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 900
+    }).reveal(`
+        #home .content a
+    `)
+
+    ScrollReveal({
+        origin: 'right',
+        distance: '40px',
+        duration: 800
+    }).reveal(`
+        #home .column-b
+    `)
+
+    ScrollReveal({
+        origin: 'bottom',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #home #stats
+    `)
+
+    //Scroll services
+    ScrollReveal({
+        origin: 'top',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #services header,
+        #services .card
+    `)
+
+    //Scroll about
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #about .column-b,
+        #about .column-a header
+    `)
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 800
+    }).reveal(`
+        #about .column-a p
+    `)
+
+    //Scroll contact
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #contact .column-b,
+        #contact header
+    `)
+
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 800
+    }).reveal(`
+        #contact #info-contacts
+    `)
+
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 900
+    }).reveal(`
+        #contact .btn-whats
+    `)
+
+    //Scroll footer
+    ScrollReveal({
+        origin: 'left',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        footer .column-a
+    `)
+    ScrollReveal({
+        origin: 'right',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        footer .column-b
+    `)
+
+}else{
+
+    console.log('menor tela');
+    ScrollReveal({
+        origin: 'top',
+        distance: '40px',
+        duration: 700
+    }).reveal(`
+        #home,
+        #home header,
+        #home .content,
+        #home img,
+        #home #statis,
+        #services,
+        #services header,
+        #services .card,
+        #about,
+        #about header,
+        #about .content p,
+        #about .content img,
+        #contact,
+        #contact header,
+        #contact .content #info-contacts,
+        #contact .content a,
+        #contact .content img,
+        footer .wrapper
+    `)
+
+}
